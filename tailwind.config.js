072dc6c0555f85b6e52ba84ed7e0e6aa900dfefe
Kishor S <kishorsbca@gmail.com>
2023-10-29 19:44:55 +0530

    bug fixed #4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // content: [
-  //   "./index.html",
-  //   "./public/firstpage.html",
-  //   "./public/login.html",
-  //   "./src/**/*.{js,ts,jsx,tsx}",
-  // ],
   content: [
     "./index.html",
-    "./public/**/*.html",
+    "./public/firstpage.html",
+    "./public/login.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  // content: [
+  //   "./index.html",
+  //   "./public/**/*.html",
+  //   "./src/**/*.{js,ts,jsx,tsx}",
+  // ],
   theme: {
     extend: {},
   },
